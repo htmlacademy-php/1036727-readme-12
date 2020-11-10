af95df4bb7e67979b6801ac70a032015c8a2d8c4
Максим <anatolev.web@gmail.com>
2020-11-10 19:01:12 +1000

    Форматирует двумерный массив $posts

--- index.php
+++ index.php
@@ -5,39 +5,39 @@ $user_name = 'Максим'; // укажите здесь ваше имя
 
 $posts = [
     [
-        'heading' => 'Цитата',
-        'type' => 'post-quote',
-        'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-        'username' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
+    'heading' => 'Цитата',
+    'type' => 'post-quote',
+    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+    'username' => 'Лариса',
+    'avatar' => 'userpic-larisa-small.jpg'
     ],
     [
-        'heading' => 'Игра престолов',
-        'type' => 'post-text',
-        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-        'username' => 'Владик',
-        'avatar' => 'userpic.jpg'
+    'heading' => 'Игра престолов',
+    'type' => 'post-text',
+    'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+    'username' => 'Владик',
+    'avatar' => 'userpic.jpg'
     ],
     [
-        'heading' => 'Наконец, обработал фотки!',
-        'type' => 'post-photo',
-        'content' => 'rock-medium.jpg',
-        'username' => 'Виктор',
-        'avatar' => 'userpic-mark.jpg'
+    'heading' => 'Наконец, обработал фотки!',
+    'type' => 'post-photo',
+    'content' => 'rock-medium.jpg',
+    'username' => 'Виктор',
+    'avatar' => 'userpic-mark.jpg'
     ],
     [
-        'heading' => 'Моя мечта',
-        'type' => 'post-photo',
-        'content' => 'coast-medium.jpg',
-        'username' => 'Лариса',
-        'avatar' => 'userpic-larisa-small.jpg'
+    'heading' => 'Моя мечта',
+    'type' => 'post-photo',
+    'content' => 'coast-medium.jpg',
+    'username' => 'Лариса',
+    'avatar' => 'userpic-larisa-small.jpg'
     ],
     [
-        'heading' => 'Лучшие курсы',
-        'type' => 'post-link',
-        'content' => 'www.htmlacademy.ru',
-        'username' => 'Владик',
-        'avatar' => 'userpic.jpg'
+    'heading' => 'Лучшие курсы',
+    'type' => 'post-link',
+    'content' => 'www.htmlacademy.ru',
+    'username' => 'Владик',
+    'avatar' => 'userpic.jpg'
     ]
 ];
 
