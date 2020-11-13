6ac857fcdeebe07f8588a9061c1a451859a2e277
Максим <anatolev.web@gmail.com>
2020-11-13 22:27:33 +1000

    Удаляет объявление функций из сценария

--- index.php
+++ index.php
@@ -2,43 +2,6 @@
 
 require_once('helpers.php');
 
-function get_text_content(string $text, int $num_letters = 300) : string {
-    $text_length = mb_strlen($text);
-
-    if ($text_length > $num_letters) {
-        $words = explode(' ', $text);
-        $result_words_length = 0;
-        $result_words = [];
-
-        foreach ($words as $word) {
-            $result_words_length += mb_strlen($word);
-
-            if ($result_words_length > $num_letters) {
-                break;
-            }
-
-            $result_words_length += 1; // +1 пробельный символ
-            $result_words[] = $word;
-        }
-
-        $result = implode(' ', $result_words);
-
-        $result .= '...';
-        $result = '<p>' . $result . '</p>';
-        $result .= '<a class="post-text__more-link" href="#">Читать далее</a>';
-    } else {
-        $result = '<p>' . $text . '</p>';
-    }
-
-    return $result;
-}
-
-function esc(string $str) : string {
-    $text = htmlspecialchars($str);
-
-    return $text;
-}
-
 $is_auth = rand(0, 1);
 
 $user_name = 'Максим'; // укажите здесь ваше имя
