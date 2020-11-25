599198bcefc5914af403482a06d907ddc11c566e
Максим <anatolev.web@gmail.com>
2020-11-25 23:35:08 +1000

    Добавляет адрес ссылке

--- templates/main.php
+++ templates/main.php
@@ -67,7 +67,7 @@
                 </blockquote>
                 <?php elseif ($post['class_name'] == 'link'): ?>
                 <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://" title="Перейти по ссылке">
+                    <a class="post-link__external" href="http://<?= esc($post['link']) ?>" title="Перейти по ссылке">
                         <div class="post-link__info-wrapper">
                             <div class="post-link__icon-wrapper">
                                 <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
