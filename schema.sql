d7204bff9918328bf222afcf99580197686acd13
Максим <anatolev.web@gmail.com>
2020-11-28 17:15:51 +1000

    Добавляет комментарии

--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
-CREATE DATABASE readme
+--
+-- База данных: readme
+--
+
+CREATE DATABASE IF NOT EXISTS readme
 	DEFAULT CHARACTER SET utf8
 	DEFAULT COLLATE utf8_general_ci;
 
 USE readme;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы user
+--
 
 CREATE TABLE user (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -15,11 +25,17 @@ CREATE TABLE user (
 	UNIQUE INDEX login(login)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы post
+--
+
 CREATE TABLE post (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	dt_add TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 	title VARCHAR(128) NOT NULL,
-	text_content TEXT NOT NULL,
+	text_content TEXT,
 	quote_author VARCHAR(128),
 	image_path VARCHAR(128),
 	video_path VARCHAR(128),
@@ -31,6 +47,12 @@ CREATE TABLE post (
 	FOREIGN KEY (content_type_id) REFERENCES content_type(id)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы comment
+--
+
 CREATE TABLE comment (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	dt_add TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
@@ -41,6 +63,12 @@ CREATE TABLE comment (
 	FOREIGN KEY (post_id) REFERENCES post(id)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы post_like
+--
+
 CREATE TABLE post_like (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	author_id INT UNSIGNED NOT NULL,
@@ -50,6 +78,12 @@ CREATE TABLE post_like (
 	UNIQUE INDEX post_like(post_id, author_id)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы subscription
+--
+
 CREATE TABLE subscription (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	author_id INT UNSIGNED NOT NULL,
@@ -57,6 +91,12 @@ CREATE TABLE subscription (
 	FOREIGN KEY (author_id) REFERENCES user(id),
 	FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы message
+--
 
 CREATE TABLE message (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -68,11 +108,23 @@ CREATE TABLE message (
 	FOREIGN KEY (recipient_id) REFERENCES user(id)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы hashtag
+--
+
 CREATE TABLE hashtag (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	hashtag_name VARCHAR(128) NOT NULL,
 	INDEX hashtag_name(hashtag_name)
 );
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы post_hashtag
+--
 
 CREATE TABLE post_hashtag (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -82,6 +134,12 @@ CREATE TABLE post_hashtag (
 	FOREIGN KEY (post_id) REFERENCES post(id)
 );
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы content_type
+--
+
 CREATE TABLE content_type (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	type_name VARCHAR(128) NOT NULL,
@@ -89,3 +147,5 @@ CREATE TABLE content_type (
 	icon_width INT UNSIGNED NOT NULL,
 	icon_height INT UNSIGNED NOT NULL
 );
+
+-- --------------------------------------------------------
